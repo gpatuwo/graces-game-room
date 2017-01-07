a2947471b0c8ae293dbe3144b8302ab657ee95ff
Grace Patuwo <grace.patuwo@gmail.com>
2017-01-06 17:22:00 -0800

    initialized board with opening pieces)

--- reversi/lib/board.js
+++ reversi/lib/board.js
@@ -5,6 +5,19 @@ let Piece = require("./piece");
  * and two white pieces at [3, 3] and [4, 4]
  */
 function _makeGrid () {
+  let grid = [];
+
+  for (var i = 0; i < 8; i++) {
+    let row = new Array(8);
+    grid.push(row);
+  }
+
+  grid[3][4] = new Piece('black');
+  grid[4][3] = new Piece('black');
+  grid[3][3] = new Piece('white');
+  grid[4][4] = new Piece('white');
+
+  return grid;
 }
 
 /**
